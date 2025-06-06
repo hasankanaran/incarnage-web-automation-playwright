109ba394ed64a03e302ff3b72c658d785de1af1c
hasankanaran <amilanarangoda1998@gmail.com>
2025-06-06 16:47:29 +0530

    Add automation script to select and add second product to cart

--- tests/ecommerce.spec.js
+++ tests/ecommerce.spec.js
@@ -7,6 +7,12 @@ const availableColour1 =
 const availableSize1 = "xpath=//label[@for='Size-2-1']";
 const addToCartButton =
   "xpath=//div[@data-fragment='add-to-cart-button']//button[@type='submit']";
+const secondProductSelector =
+  "xpath=//main[@id='main']//div[@id='ProductCard-7536862789838']//picture[1]";
+const availableColour2 =
+  "xpath=//div[@class='color-swatch-item color-swatch-jet-black']";
+const availableSize2 = "xpath=//label[@for='Size-2-2']";
+
 
 test("Navigate and add products to the cart", async ({ page }) => {
   // Step 1: Go to the homepage and wait for full load
@@ -15,20 +21,31 @@ test("Navigate and add products to the cart", async ({ page }) => {
   // Step 2: Click on the product card
   await page.locator(firstProductSelector).click();
 
-
   // Step 3: Select color
   await page.locator(availableColour1).click();
 
   // Step 4: Select size
-  await page.locator(availableSize1).click(); 
+  await page.locator(availableSize1).click();
 
   // Step 5: Add to cart;
-await page.locator(addToCartButton).click();
-await page.waitForTimeout(3000); // Wait for 3 seconds to ensure the cart updates
+  await page.locator(addToCartButton).click();
+  await page.waitForTimeout(3000); // Wait for 3 seconds to ensure the cart updates
 
   // Step 6: Return to homepage
   await page.goto(baseURL, { waitUntil: "load" });
 
-  // Debug or pause here if needed
+  // Step 7: Click on the product card again
+  await page.locator(secondProductSelector).click();
+
+  // Step 8: Select color
+  await page.locator(availableColour2).click();
+
+  // Step 9: Select size
+  await page.locator(availableSize2).click();
+
+  // Step 10: Add to cart
+  await page.locator(addToCartButton).click();
+  await page.waitForTimeout(3000); // Wait for 3 seconds to ensure the cart updates
+
   await page.pause();
 });
