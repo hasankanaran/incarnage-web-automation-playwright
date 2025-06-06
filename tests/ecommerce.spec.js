e2d1c9f133e0af4c1af2dbd84af6c084e27b442f
hasankanaran <amilanarangoda1998@gmail.com>
2025-06-06 20:36:02 +0530

    Add automation script to add Note and click on checnkout page

--- tests/ecommerce.spec.js
+++ tests/ecommerce.spec.js
@@ -1,19 +1,33 @@
 import { test } from "@playwright/test";
 
 const baseURL = "https://incarnage.com/";
-const addToCartButton = "xpath=//div[@data-fragment='add-to-cart-button']//button[@type='submit']";
-const cartVisible = "xpath=//body/div[@id='SlideoutCart']/div[@class='slideout-cart-container visible']/div[@class='cart']/div[3]";
+const addToCartButton =
+  "xpath=//div[@data-fragment='add-to-cart-button']//button[@type='submit']";
+const cartVisible =
+  "xpath=//body/div[@id='SlideoutCart']/div[@class='slideout-cart-container visible']/div[@class='cart']/div[3]";
+
+const btnShowNotes = "xpath=//label[@for='showNote']";
+
+const noteTextArea = "xpath=//textarea[@placeholder='Enter your note here...']";
+
+const btnCheckOut = "xpath=//button[@name='checkout']";
 
 const firstProductSelector = "xpath=//div[@id='ProductCard-7536860430542']";
-const availableColour1 = "xpath=//div[@class='color-swatch-item color-swatch-sheer-white']";
+const availableColour1 =
+  "xpath=//div[@class='color-swatch-item color-swatch-sheer-white']";
 const availableSize1 = "xpath=//label[@for='Size-2-1']";
 
-const secondProductSelector = "xpath=//main[@id='main']//div[@id='ProductCard-7536862789838']//picture[1]";
-const availableColour2 = "xpath=//div[@class='color-swatch-item color-swatch-jet-black']";
+const secondProductSelector =
+  "xpath=//main[@id='main']//div[@id='ProductCard-7536862789838']//picture[1]";
+const availableColour2 =
+  "xpath=//div[@class='color-swatch-item color-swatch-jet-black']";
 const availableSize2 = "xpath=//label[@for='Size-2-2']";
 
-const thirdProductSelector = "//div[@id='ProductCard-7575887151310']//picture[1]";
-const availableColour3 = "xpath=//div[@class='color-swatch-item color-swatch-navy-blue']";  
+const thirdProductSelector =
+  "xpath=//div[@id='ProductCard-7575887151310']//picture[1]";
+
+const availableColour3 =
+  "xpath=//div[@class='color-swatch-item color-swatch-navy-blue']";
 const availableSize3 = "xpath=//div[normalize-space()='l']";
 
 test("Navigate and add products to the cart", async ({ page }) => {
@@ -23,9 +37,9 @@ test("Navigate and add products to the cart", async ({ page }) => {
   await page.locator(availableColour1).click();
   await page.locator(availableSize1).click();
   await page.locator(addToCartButton).click();
-  await page.locator(cartVisible).waitFor({ state: "visible" });  // keep this one to wait for cart update
+  await page.locator(cartVisible).waitFor({ state: "visible" }); //  wait for cart update the cart
 
-  // Second Product 
+  // Second Product
   await page.goto(baseURL);
   await page.locator(secondProductSelector).click();
   await page.locator(availableColour2).click();
@@ -40,6 +54,16 @@ test("Navigate and add products to the cart", async ({ page }) => {
   await page.locator(availableSize3).click();
   await page.locator(addToCartButton).click();
   await page.locator(cartVisible).waitFor({ state: "visible" });
+
+  //leaving a note with order
+  await page.locator(btnShowNotes).click();
+  await page
+    .locator(noteTextArea)
+    .fill(
+      "Intern QA Test:I’m Hasanka Narangoda,eager to learn and help with QA and development. Thank you Zitles for this opportunity."
+    );
+    
+  await page.locator(btnCheckOut).click();
 
   await page.pause();
 });
